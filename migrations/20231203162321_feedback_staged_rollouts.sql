200196825d5e21df4e8fe25e27c0d9443508a8c2
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2023-12-04 00:25:55 +0100

    Implement simple feedback-counter website

---
+++ migrations/20231203162321_feedback_staged_rollouts.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS feedback_staged_rollouts (
+    id INTEGER PRIMARY KEY NOT NULL,
+    date DATE NOT NULL
+);
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use std::env;
+use std::path::PathBuf;
+use std::sync::Arc;
+use askama::Template;
+use axum::{extract::State, routing::get, Router};
+use axum::http::StatusCode;
+use sqlx::{Sqlite, SqlitePool};
+use sqlx::migrate::MigrateDatabase;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexHtml {}
+
+#[derive(Template)]
+#[template(path = "evaluation.html")]
+struct EvaluationTemplate {}
+
+async fn index(State(state): State<Arc<AppState>>) -> Result<IndexHtml, StatusCode> {
+    let mut db = state.db_pool.acquire().await.unwrap();
+    let time = chrono::offset::Utc::now();
+    let success = sqlx::query!(
+        r#"INSERT INTO feedback_staged_rollouts (date) VALUES (?1)"#,
+        time
+    )
+        .execute(&mut *db)
+        .await.is_ok();
+    if success {
+        Ok(IndexHtml {})
+    } else {
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+async fn evaluation(State(state): State<Arc<AppState>>) -> Result<EvaluationTemplate, StatusCode> {
+
+}
+
+async fn setup_db(db_url: &str, migrations_path: PathBuf) -> Result<sqlx::Pool<Sqlite>, sqlx::Error> {
+
+    if !Sqlite::database_exists(db_url).await.unwrap_or(false) {
+        println!("Creating database...");
+        Sqlite::create_database(db_url).await?;
+    }
+    let db = SqlitePool::connect(db_url).await.unwrap();
+    let migration_results = sqlx::migrate::Migrator::new(migrations_path)
+        .await
+        .unwrap()
+        .run(&db)
+        .await;
+    match migration_results {
+        Ok(_) => println!("Migration success"),
+        Err(error) => {
+            panic!("error: {}", error);
+        }
+    }
+    Ok(db)
+}
+
+struct AppState {
+    db_pool: SqlitePool
+}
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error>{
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let migrations = std::path::Path::new(&crate_dir).join("./migrations");
+    let db_url = match env::var("DATABASE_URL") {
+        Ok(val) => val,
+        Err(_) => String::from("sqlite:memory")
+    };
+    let db = setup_db(&db_url, migrations).await?;
+
+    let shared_state = Arc::new(AppState{
+       db_pool: db
+    });
+
+    let app = Router::new()
+        .route("/", get(index))
+        .with_state(shared_state);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
